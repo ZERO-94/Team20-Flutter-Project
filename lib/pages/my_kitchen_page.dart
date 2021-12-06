bd5a33c6d72010e501127a7127eaeb055ee98a03
ZERO-94 <kimanh.tvka@gmail.com>
2021-12-06 20:37:19 +0700

    create my_kitchen_page

---
+++ lib/pages/my_kitchen_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyKitchenPage extends StatefulWidget {
+  const MyKitchenPage({Key? key}) : super(key: key);
+
+  @override
+  _MyKitchenPageState createState() => _MyKitchenPageState();
+}
+
+class _MyKitchenPageState extends State<MyKitchenPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
